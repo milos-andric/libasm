e845ce079f2ffdbd460352d1bd5b02a14dbbf30b
user42 <milos.andrc@gmail.com>
2020-09-24 22:15:21 +0200

    quick fix read write

--- ft_read.s
+++ ft_read.s
@@ -1,9 +1,6 @@
 section .text
 global ft_read
 
-%define s1 rdi
-%define s2 rsi
-
 ft_read :
 xor			rax, rax
 mov			rax, 0x00
--- ft_write.s
+++ ft_write.s
@@ -1,9 +1,6 @@
 section .text
 global ft_write
 
-%define s1 rdi
-%define s2 rsi
-
 ft_write :
 xor			rax, rax
 mov			rax, 0x01
