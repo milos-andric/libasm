9eed8eefc1d848b4a3d2dcdb6f6be6ff89dd96c4
milos <milos@mandric>
2020-09-26 19:29:53 +0200

    adding strdup but not compiling

---
+++ ft_strdup.s
@@ -0,0 +1,32 @@
+; On recoit les parametres dans l ordre des registres suivant :
+; Rdi, ici dest
+; Rsi, ici src
+; Rdx
+; R10
+; R8
+; R9
+; ils sont tous de taille 32bits
+
+section .text
+extern malloc, ft_strlen, ft_strcpy
+global ft_strdup
+
+ft_strdup:
+push    rdi
+call    strlen
+inc     rax
+mov     rdi, rax
+call    malloc
+cmp     rax, 0x0
+jne     _end
+
+_error:
+mov     rax, 0x0
+ret
+
+_end:
+pop     rdi
+mov     rsi, rdi
+mov     rdi, rax
+call    ft_strcpy
+ret
